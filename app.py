d028a42430b5f53c1c4df6aaadae9b986dad9957
cruzdany <al1609073@upy.edu.mx>
2020-12-03 01:57:24 -0600

    Upload zip

--- app.py
+++ app.py
@@ -4,19 +4,22 @@ import numpy as np
 import joblib
 import traceback
 from flask_restful import reqparse
+
 app = Flask(__name__)
 
-"""@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET'])
 def hello():
-    return "hey"""
+    return "hey"
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
 	lr = joblib.load("model.pkl")
 	if lr:
 		try:
-			json = request.get_json()	 
+			json = request.get_json()
+			#print(json)	 
 			model_columns = joblib.load("model_cols.pkl")
+			#print(model_columns)
 			temp=list(json[0].values())
 			vals=np.array(temp)
 			prediction = lr.predict(temp)
@@ -28,7 +31,5 @@ def predict():
 	else:
 		return ('No model here to use')
     
-
-
 if __name__ == '__main__':
     app.run(debug=True)
